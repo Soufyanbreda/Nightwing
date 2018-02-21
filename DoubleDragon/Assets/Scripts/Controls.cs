041e237bb7edea858924f5d4093d716597bbdd98
Soufyanbreda <Soufyanbargach@MacBook-Pro-van-Soufyan.local>
2018-02-21 11:31:53 +0100

    Added contral classe
    want to make my avitar move.

---
+++ DoubleDragon/Assets/Scripts/Controls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float movespeed;
+
+	// Use this for initialization
+	void Start () {
+	rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+		}
+	if (Input.GetKey (KeyCode.RightArrow))
+		{
+			rb.velocity = new Vector2(movespeed, rb.velocity.y);
+		}
+	}
+}
