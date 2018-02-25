f9e16a5f07a1f8edca4cafc2f850c529c3c308ed
Soufyanbreda <Soufyanbargach@MacBook-Pro-van-Soufyan.local>
2018-02-25 22:00:21 +0100

    Implemented shooting
    Still work in progress.

---
+++ DoubleDragon/Assets/Scripts/Destroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	public float delay = 1f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, delay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- DoubleDragon/Assets/Scripts/Shootsomething.cs
+++ DoubleDragon/Assets/Scripts/Shootsomething.cs
@@ -6,7 +6,7 @@ public class Shootsomething : MonoBehaviour {
 	public GameObject projectile;
 	public Vector2 velocity;
 	public bool canShoot = true;
-	public Vector2 offset;
+	public Vector2 offset = new Vector2 (0.4f, 0.1f);
 	public float cooldown = 1f;
 
 
@@ -18,11 +18,12 @@ public class Shootsomething : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		if (Input.GetKeyDown (0) && canShoot)
+		if (Input.GetKeyDown (KeyCode.T) && canShoot)
 		{
+			//StartCoroutine(CanShoot);
 			GameObject go =	(GameObject) Instantiate (projectile, (Vector2)transform.position + offset * transform.localScale.x, Quaternion.identity);
-
 			go.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x * transform.localScale.x,velocity.y);
+
 		}
 	}
 
